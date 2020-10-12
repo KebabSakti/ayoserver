a6188d98c2dda296378ac6be76be8ce3d4f9a2c5
Julian Aryo <julian.aryo1989@gmail.com>
2020-10-12 19:33:59 +0800

    upload

---
+++ database/migrations/2020_10_12_070705_create_delivery_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliveryDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('delivery_detail_id');
+            $table->string('sale_id');
+            $table->string('customer_id');
+            $table->string('courier_id');
+            $table->string('name');
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery_details');
+    }
+}
